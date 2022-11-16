06f07fa630bab9f14e0c42978b05afb1fcf5fc41
Dziewczyna <joanna.podkoscielna@gmail.com>
2022-11-16 21:27:17 +0100

    Initial commit of CRUD application

---
+++ src/main/java/com/springApp/movielibrary/MovieRepository.java
@@ -0,0 +1,49 @@
+package com.springApp.movielibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+
+  @Autowired JdbcTemplate jdbcTemplate;
+
+  public List<Movie> getAll() {
+    return jdbcTemplate.query(
+        "SELECT id, name, rating FROM movie", BeanPropertyRowMapper.newInstance(Movie.class));
+  }
+
+  public Movie getMovieById(int movieId) {
+    return jdbcTemplate.queryForObject(
+        "SELECT id,name,rating FROM movie WHERE id=?",
+        BeanPropertyRowMapper.newInstance(Movie.class),
+        movieId);
+  }
+
+  public int save(List<Movie> movies) {
+    movies.forEach(
+        movie ->
+            jdbcTemplate.update(
+                "INSERT INTO movie(name, rating) VALUES(?, ?)",
+                movie.getName(),
+                movie.getRating()));
+
+    return 1;
+  }
+
+  public int update(Movie movie) {
+    return jdbcTemplate.update(
+        "UPDATE movie SET name=?, rating=? WHERE id=?",
+        movie.getName(),
+        movie.getRating(),
+        movie.getId());
+  }
+
+  public int delete(int movieId) {
+    return jdbcTemplate.update("DELETE FROM movie WHERE id=?", movieId);
+  }
+}
